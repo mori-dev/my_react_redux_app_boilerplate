f2ac7d0ab07533021947bb143db0e65f9b520ce8
mori-dev <mori.dev.asdf@gmail.com>
2016-07-06 17:57:49 +0900

    chore: .babelrc の廃止

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,10 @@ module.exports = {
       {
         test: /.jsx?$/,
         loader: 'babel',
-        exclude: /node_modules/
+        exclude: /node_modules/,
+        query:{
+          presets: ["react", "es2015", "stage-0"]
+        }
       },
       {
         test: /.css$/,
