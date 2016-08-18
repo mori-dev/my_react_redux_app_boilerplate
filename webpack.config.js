d1dcdbbd2612006fda07802262a2240ac8e66c97
mori-dev <mori.dev.asdf@gmail.com>
2016-08-18 13:55:48 +0900

    refactor: インデントの調整など

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,45 @@
 path = require('path');
 webpack = require('webpack');
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve('dist/'),
-    filename: 'bundle.js'
-  },
-  module: {
-    preLoaders: [
-      { test: /\.js$/, loader: 'eslint-loader', exclude: /node_modules/ },
+const config = {
+    entry: ['./src/index.js'],
+    output: {
+        path: path.resolve('dist/'),
+        filename: 'bundle.js'
+    },
+    module: {
+        preLoaders: [
+            { test: /\.js$/, loader: 'eslint-loader', exclude: /node_modules/ },
+        ],
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: 'babel',
+                exclude: /node_modules/,
+            },
+            {
+                    test: /\.css$/,
+                    loaders: ['style', 'css?modules'],
+            },
+        ]
+    },
+    eslint: {
+            configFile: './.eslintrc',
+            fix: true,
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+            },
+        }),
     ],
-    loaders: [
-      {
-        test: /\.js$/,
-        loader: 'babel',
-        exclude: /node_modules/,
-      },
-      {
-          test: /\.css$/,
-          loaders: ['style', 'css?modules'],
-      },
-    ]
-  },
-  eslint: {
-      configFile: './.eslintrc',
-      fix: true,
-  },
-  devServer: {
-    contentBase: 'dist',
-      port: 8080,
-      host: "0.0.0.0",
-      inline: true,
-  }
+    devServer: {
+        contentBase: 'dist',
+            port: 8080,
+            host: "0.0.0.0",
+            inline: true,
+    },
 }
+
+module.exports = config;
